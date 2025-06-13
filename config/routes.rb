516f3622ecd4b8bfc13d97f4f636f04ccd45910c
lesson-t <goozooloo123@gmail.com>
2025-06-13 10:40:06 +0900

    Create whisky-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'whiskies/index'
+  get 'whiskies/show'
+  get 'whiskies/new'
+  get 'whiskies/edit'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
