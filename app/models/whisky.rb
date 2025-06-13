ff145de1ffd97186cceb981c6e60006c414d6398
lesson-t <goozooloo123@gmail.com>
2025-06-13 10:32:18 +0900

    Add enum and validations to Whisky model

--- app/models/whisky.rb
+++ app/models/whisky.rb
@@ -1,3 +1,8 @@
 class Whisky < ApplicationRecord
   belongs_to :user
+
+  validates :name, :genre, :country, :status, presence: true
+  validates :rating, inclusion: { in: 1..5 }, allow_nil: true
+
+  enum status: { 未飲: 0, 飲んだ: 1 }
 end
