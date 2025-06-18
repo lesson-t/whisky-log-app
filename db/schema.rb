d7b5594078541baedf9ba9b578320f2e40864e95
lesson-t <goozooloo123@gmail.com>
2025-06-19 06:26:00 +0900

    feat:Create Favorite model to manage user-whisky favorites

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_13_021947) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_18_212331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_13_021947) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "whisky_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+    t.index ["whisky_id"], name: "index_favorites_on_whisky_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -70,5 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_13_021947) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "favorites", "whiskies"
   add_foreign_key "whiskies", "users"
 end
---
+++ spec/factories/favorites.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :favorite do
+    user { nil }
+    whisky { nil }
+  end
+end
